c510449bbe0183ca42a83fb684f56ba9e7f436e1
Darnell Baird <drsbaird@yahoo.com>
2021-08-24 12:15:43 -0400

    refactor: Rename folder 'auth' to 'user'

--- PARAM.py
+++ PARAM.py
@@ -1,10 +1,11 @@
 class HTML:
     HOME = "index.jinja-html"
-    #auth
-    REGISTER = "/auth/register.jinja-html"
-    LOGIN = "auth/login.jinja-html"
-    LOGOUT = "/auth/logout.jinja-html"
-    MESSAGE = "auth/message.jinja-html"
+    #user
+    REGISTER = "/user/register.jinja-html"
+    LOGIN = "user/login.jinja-html"
+    LOGOUT = "/user/logout.jinja-html"
+    SETTINGS = "user/settings.jinja-html"
+    MESSAGE = "user/message.jinja-html"
 
 class CONFIG:
     APP = "app.cfg"
