238b428f1cee04e1f71944e80b8ec0b1fad2293d
Darnell Baird <drsbaird@yahoo.com>
2021-09-09 17:29:21 -0400

    test: Working unittest

--- tests/test_model.py
+++ tests/test_model.py
@@ -4,7 +4,7 @@ import flask
 
 import app as App
 import app.model as app_model
-import app.db as db
+import app.db as DB
 
 from . import TempDatabaseHandler
 from instance import config as instance_config
@@ -20,30 +20,48 @@ class TestUser(unittest.TestCase):
     def tearDownClass(cls):
         del cls.temp_db_handler #must be explicit
 
+    def setUp(self):
+        self.app = App.create_app(test_config=True)
+        self.test_client = self.app.test_client()
+
     def test_users(self):
-        """Set and Get users from the database"""
-        app = App.create_app()
+        """Insert, Select, and Delete users from the database"""
         user1 = app_model.User(
             username="John Smith", 
             password="secret m3!",
             email="john@example.com",
             )
-        with app.app_context():
-            db.insert_user(user1)
-            user1_row = db.get_user_from_username("John Smith")
-            user1_ret = app_model.User.from_row(user1_row)
-            self.assertEqual(user1, user1_ret)
+        with self.app.test_request_context():
+            # Insert user
+            app_model.User.DB.insert_user(user1)
+            # self.assertTrue(app_model.DB.email_exists(user1.email))   #Not tested as yet
+            # Get User
+            user1_ret_email = app_model.User.DB.get_user_from_email(user1.email)
+            user1_ret_username = app_model.User.DB.get_user_from_username(user1.username)
+            user1_ret_username_email_email = app_model.User.DB.get_user_from_username_email(user1.email)
+            user1_ret_username_email_username = app_model.User.DB.get_user_from_username_email(user1.username)
+            self.assertEqual(user1, user1_ret_email)
+            self.assertEqual(user1, user1_ret_username)
+            self.assertEqual(user1, user1_ret_username_email_email)
+            self.assertEqual(user1, user1_ret_username_email_username)
+            # Delete User 
+            # (ATM, no function). clean-up
+            # sql = "DELETE FROM Users WHERE email=john@example.com"
+            sql = "DELETE FROM Users WHERE user_id = (SELECT MAX(user_id) FROM Users);"
+            DB.get_db().execute(sql)
+            DB.get_db().commit()
 
     def test_admin(self):
-        #Assert Admin User
+        # Admin User exists and is valid
         sql = "SELECT username, salt_password, email, is_active, is_admin FROM Users WHERE is_admin=1;"
-        connection = db.get_db()
-        rows = connection.execute(sql)
-        n_admins = 0
-        for i, row in enumerate(rows):
-            n_admins += 1
-            admin_row = row
-            print(row['is_admin'])
-        self.assertEqual(n_admins, 1, msg="There should only be one admin.")
-        self.assertEqual(admin_row['is_admin'], 1)
-        self.assertEqual(admin_row['username'], 'admin')
+        with self.app.test_request_context():
+            connection = DB.get_db()
+            rows = connection.execute(sql)
+            n_admins = 0
+            for i, row in enumerate(rows):
+                n_admins += 1
+                admin_row = row
+                print(row['is_admin'])
+            self.assertEqual(n_admins, 1, msg="There should only be one admin.")
+            self.assertEqual(admin_row['is_admin'], 1)
+            self.assertEqual(admin_row['username'], 'admin')
