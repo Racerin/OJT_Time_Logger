9d0a9be80ff8427264832fa9de50fc2d42dcf59a
Darnell Baird <drsbaird@yahoo.com>
2021-09-22 19:47:35 -0400

    refactor: Reduced/Simplify amount of tests

--- tests/test_user.py
+++ tests/test_user.py
@@ -10,7 +10,7 @@ from instance import config as instance_config
 import app.user as app_user
 
 
-class TestPages(unittest.TestCase):
+class TestUser(unittest.TestCase):
 
     vldr_msgs = app_user.UserRegisterForm._vldr_msgs
 
@@ -20,7 +20,6 @@ class TestPages(unittest.TestCase):
     def test_register(self):
         """Test user register page."""
         # Set up
-        self.app.config["WTF_CSRF_ENABLED"] = False #Disable CSRF authentication
         client = self.app.test_client()
         # 1st time, Register page
         resp = client.get("user/register")
@@ -59,7 +58,6 @@ class TestPages(unittest.TestCase):
         Use 'test_register' for reference.
         """
         # Set up
-        self.app.config["WTF_CSRF_ENABLED"] = False #Disable CSRF authentication
         client = self.app.test_client()
         # Post a valid email
         with self.app.test_request_context():
@@ -99,84 +97,46 @@ class TestPages(unittest.TestCase):
                 assert "Success" not in html, data
                 check(html) #assertion
 
-
-class TestUser(unittest.TestCase):
-
-    def setUp(self) -> None:
-        self.app = App.create_app(test_config=True)
-    
-    def test_sign_in_out(self):
-        """Ensure user can log in and out, and is with correct privileges."""
+    def test_user_lifecycle(self):
+        """Test processes of user registration, login, logout and deregistration.
+        """
         client = self.app.test_client()
-        #GET user login webpagepage
-        resp = client.get(
-            "user/login", 
-            content_type='html/text'
-            )
-        # print("Flask request data:", resp.request.data)
+        # Register
+        resp = client.get('user/register')
         self.assertEqual(resp.status_code, 200)
-        # self.print_response_options(get_login_resp)
-        session = client.session_transaction()
-        # print("This is after client's GET session:", session, "session dir:", dir(session))
-        #POST login user webpage
-        post_data = {
-            'username':"FooBar",
-            'password':instance_config.FOO_USER_PASSWORD,
-            # 'password':'dog',
-            "login_form":"",
-        }
-        post_login_resp = client.post(
-            "user/login", 
-            data=post_data, 
-            follow_redirects=True,
-            )
-        # self.print_response_options(post_login_resp, print_dir=False)
-        self.assertEqual(post_login_resp.status_code, 200)
-        """ self.assertNotEqual(
-            get_login_resp.data, 
-            post_login_resp.data,
-            msg="The webpage between post and get methods is the same."
-            )
-        self.assertTrue(
-            b'Success' in post_login_resp.data or b'success' in post_login_resp.data,
-            msg="This is not the success page."
-            ) """
-
-    def test_sign_in_out2(self):
-        with self.app.test_client() as client:
-            with client.session_transaction() as session:
-                pass
-                # print("Client Session:", session)
-        get_request = self.app.test_request_context()
-        get_request.push()
-        get_request.pop()
-
-    def test_register(self):
-        """Register a user."""
-        pass
-
-    def print_response_options(self, response, print_dir=True):
-        if print_dir:
-            print(f"Response '{response}' dir: {dir(response)}")
-        info = [
-            'status', 'status_code',
-            'history',
-            'expires',
-            'data',
-            'headers',
-        ]
-        print("="*50)
-        for tag in info:
-            rep_tag = tag.replace('_',' ').title()
-            print(f"{rep_tag}: {getattr(response, tag)}")
+        # get string html data of webpage
+        html = resp.data.decode()
+        self.assertIn("Register", html)
+        with self.app.test_request_context():
+            form = app_user.UserRegisterForm(data=dict(
+                username='Bobbylite',
+                email='bob@example.com',
+                password1='Password@1234',
+                password2='Password@1234'
+            ))
+            resp = client.post("user/register", data=form.data, follow_redirects=True)
+            html = resp.data.decode()
+            self.assertEqual(resp.status_code, 200)
+            assert 'Register' not in html, html
+            assert 'Success' in html, html
+        # Login
+        with self.app.test_request_context():
+            form = app_user.UserLoginForm(data=dict(
+                username_email='Bobbylite',
+                password='Password@1234',
+            ))
+            resp = client.post("user/login", data=form.data, follow_redirects=True)
+            html = resp.data.decode()
+            self.assertEqual(resp.status_code, 200)
+            assert 'Login' not in html, html
+            assert 'Logout' in html, html
 
 
 class TestAdminUser(TestUser):
 
     def test_sign_in_out(self):
         """Ensure admin could sign in and out with correct privilages?"""
-        with self.app.test_client() as client:
-            pass
+        client = self.app.test_client()
 
     def test_privileges(self):
         pass
