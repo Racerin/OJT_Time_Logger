d46c383ff9e1904b249c9225f39c2e1d437a0026
Darnell Baird <drsbaird@yahoo.com>
2021-09-30 18:24:08 -0400

    feat: Add 'experiment' webpage

--- app/views.py
+++ app/views.py
@@ -7,4 +7,7 @@ def init_app(app):
     @app.route('/')
     def home():
         return flask.render_template(PARAM.HTML.HOME)
-        # return "All my exes live in Texas."
+
+    @app.route('/experiment')
+    def experiment():
+        return flask.render_template(PARAM.HTML.EXPERIMENT)
