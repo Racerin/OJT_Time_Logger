2294504df0810670d180e69203f5b066f8b18935
Darnell Baird <drsbaird@yahoo.com>
2021-10-03 23:37:38 -0400

    feat: Add js tab selector script

---
+++ app/static/js/user/settings.js
@@ -0,0 +1,28 @@
+// Settings Tabs
+const tabs = document.querySelectorAll('.tabs li');
+const tabContents = document.querySelectorAll('#tab-content > div');
+
+function selectActive(){};
+function selectDetail(){};
+
+// tabs.addEventListener('down', selectActive);
+tabs.forEach(tab => {
+    tab.addEventListener('click', ()=> {
+        // Select active tab
+        tabs.forEach(item => item.classList.remove('is-active'));
+        tab.classList.add('is-active')
+
+        // Set the content
+        const target = tab.dataset.target;
+        console.log("This is the target", target);
+        tabContents.forEach(tabContent => {
+            // if (tabContent.getAttribute('id') == target){
+            if (tabContent.id == target){
+                tabContent.classList.remove('is-hidden');
+            }
+            else {
+                tabContent.classList.add('is-hidden');
+            };
+        });
+    })
+});
